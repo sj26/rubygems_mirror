81cd3a05e9556c65d5a4df85e10b48f2a67712bf
Samuel Cochran <sj26@sj26.com>
2014-05-11 14:01:43 +1000

    Redirect project names to latest version

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,4 +1,14 @@
 class VersionsController < ApplicationController
+  before_action :redirect_bare_project
+
   def show
+  end
+
+private
+
+  def redirect_bare_project
+    if project = Project.find_by_name(params[:id])
+      redirect_to project.latest_version
+    end
   end
 end
