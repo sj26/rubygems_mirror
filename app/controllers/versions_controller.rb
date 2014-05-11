8eb53f5ea6fb85f46fb934a492fb964ed3b8834c
Samuel Cochran <sj26@sj26.com>
2014-05-11 19:33:11 +1000

    Move browse and raw onto versions controller

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,6 +1,7 @@
 class VersionsController < ApplicationController
   before_action :redirect_bare_project
   before_action :prepare_version
+  before_action :prepare_package, only: [:browse, :raw]
 
   def show
     respond_to do |format|
@@ -13,6 +14,15 @@ class VersionsController < ApplicationController
   def other
   end
 
+  def browse
+  end
+
+  def raw
+    @package.data_file(@path) do |file|
+      send_data file.data, filename: file.name, type: file.content_type, disposition: file.disposition
+    end
+  end
+
 private
 
   def redirect_bare_project
@@ -23,5 +33,12 @@ private
 
   def prepare_version
     @version = Version.find_by_full_name!(params[:id])
+  end
+
+  def prepare_package
+    @package = @version.package
+    @path = request.path_parameters[:path].to_s
+    @entry = @package[@path]
+    raise "not found" unless @entry.present?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   resources :projects, path: :gems, only: [:index]
 
   resources :versions, path: :gems, only: [:show], constraints: {id: Patterns::NONGREEDY_SLUG, format: /html|json|gem|gemspec/} do
-    get :other, on: :member
-
-    resource :package, path: "", only: [] do
+    member do
+      get :other
       get :browse, path: "browse(/*path)"
+      get :raw, path: "raw/*path"
     end
   end
 
