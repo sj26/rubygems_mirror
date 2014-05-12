b6094cdffc6f7cf3e8a8c8d1d2438db402eea735
Samuel Cochran <sj26@sj26.com>
2014-05-12 14:13:05 +1000

    Tweak routes, add download links on versions show

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   get "gems/:id(.:format)", to: "versions#show", as: :version, constraints: {id: Patterns::NONGREEDY_SLUG, format: /html|json/}
   get "gems/:id(.:format)/other", to: "versions#other", as: :other_version, constraints: {id: Patterns::NONGREEDY_SLUG, format: /html|json/}
 
-  get "gems/:version_id(.:format)", to: "package#show", as: :package, constraints: {version_id: Patterns::NONGREEDY_SLUG, format: /gem/}
-  get "gems/:version_id(.:format)", to: "package#specification", as: :package_specification, constraints: {version_id: Patterns::NONGREEDY_SLUG, format: /gemspec|ya?ml/}
+  get "gems/:version_id(.:format)", to: "package#show", as: :version_package, defaults: {format: "gem"}, constraints: {version_id: Patterns::NONGREEDY_SLUG, format: /gem/}
+  get "gems/:version_id(.:format)", to: "package#specification", as: :version_specification, defaults: {format: "gemspec"}, constraints: {version_id: Patterns::NONGREEDY_SLUG, format: /gemspec|ya?ml/}
   get "gems/:version_id/browse(/*path)", to: "packages#browse", as: :browse_version, constraints: {version_id: Patterns::NONGREEDY_SLUG, path: /.+/}
   get "gems/:version_id/raw(/*path)", to: "packages#raw", as: :raw_version, constraints: {version_id: Patterns::NONGREEDY_SLUG, path: /.+/}
 
